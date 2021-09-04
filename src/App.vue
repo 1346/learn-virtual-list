b3532b3a8ca53deda43b94fa7c6ec819c83efbb3
1346 <961131804@qq.com>
2021-09-04 20:06:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view /> -->
+    <!-- <virtual-list :listData="data" :itemSize="100"></virtual-list> -->
+    <virtual-list-update
+      :listData="data"
+      :estimatedItemSize="100"
+      v-slot="slotProps"
+    >
+      <item :item="slotProps.item" />
+    </virtual-list-update>
+  </div>
+</template>
+
+<script>
+import VirtualList from "./components/VirtualList.vue";
+import VirtualListUpdate from "./components/VirtualListUpdate/VirtualListUpdate.vue";
+import faker from "faker";
+import Item from "./components/VirtualListUpdate/Item.vue";
+
+let data = [];
+for (let i = 0; i < 1000; i++) {
+  data.push({
+    i,
+    value: faker.lorem.sentences() // mock长文本出来
+  });
+}
+
+export default {
+  components: { VirtualList, VirtualListUpdate, Item },
+  name: "App",
+  data() {
+    return {
+      data
+    };
+  }
+};
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  /* font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+  height: 100%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <!-- <h1>{{ msg }}</h1>
+    <div v-for="item of 10000" :key="item">
+      {{ item }}
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+</style>
